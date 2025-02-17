eb0bee1ae3a0daab43417ae88674f54108488a4c
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2025-02-17 17:38:11 +0800

    Update php-handler.js

--- api/php-handler.js
+++ api/php-handler.js
@@ -2,14 +2,16 @@ const phpServer = require("php-server");
 const path = require("path");
 
 module.exports = (req, res) => {
-  // Start PHP server and serve the PHP files
-  phpServer({
+  // Start PHP server only if it's not already running
+  const server = phpServer({
     port: 8080,
     root: path.join(__dirname, "../public"),
     open: false,
     quiet: true,
   });
 
-  // Respond to requests from Vercel
-  res.status(200).send("PHP server started");
+  // Allow the server to continue running
+  server.listen(8080, () => {
+    res.status(200).send("PHP server started");
+  });
 };
